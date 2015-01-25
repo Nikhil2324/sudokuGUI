b564a22b7993e4074edb5654c468a36ff49f1dd3
nikhil2324 <nikhil2324@g.ucla.edu>
2015-01-25 04:12:53 -0800

    prints to idle, need to print to GUI

--- board.py
+++ board.py
@@ -4,8 +4,89 @@ MARGIN = 20  # Pixels around the board
 SIDE = 50  # Width of every board cell.
 WIDTH = HEIGHT = MARGIN * 2 + SIDE * 9  # Width and height of the whole board
 
-class SudokuUI(Frame):
+def mapping(row, col):
+    return [a+b for a in row for b in col]
 
+digits = '123456789'
+rows = 'ABCDEFGHI'
+cols = digits
+squares = mapping(rows, cols)
+unitlist = ([mapping(rows, c) for c in cols] + [mapping(r, cols) for r in rows] + [mapping(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]) #cols, rows, boxes
+units = dict((square, [u for u in unitlist if square in u]) for square in squares) #col, row, and box for each square
+peers = dict((square, set(sum(units[square],[]))-set([square])) for square in squares) #unit for each square in unit of s, but not s
+
+#possibilities is dict mapping each square to every value it can have
+#possibilities[s] is specific square
+
+def parse_grid(board):
+    possibilities = dict((square, digits) for square in squares) #each square has every possible answer
+    for square,digit in grid_values(board).items():
+        if digit in digits and not assign(possibilities, square, digit): #assign if d is a digit
+            return False
+    return possibilities
+
+def grid_values(board):
+    values = [c for c in board if c in digits or c in '0.'] #list of squares values (0 or . for empty/unknown squares)
+    return dict(zip(squares, values)) #map each square to its value
+
+def assign(possibilities, square, digit):
+    other_possibilities = possibilities[square].replace(digit, '') #other_values is every possiblity except digit
+    if all(remove(possibilities, square, d2) for d2 in other_possibilities): #if those values are all invalid, found answer
+        return possibilities
+    else:
+        return False
+
+def remove(possibilities, square, digit): #eliminate d from values[s]
+    if digit not in possibilities[square]:
+        return possibilities #eliminated
+    
+    possibilities[square] = possibilities[square].replace(digit, '') #remove possibility from square
+    
+    if len(possibilities[square]) == 0:
+        return False #removed last value, should be impossible
+    
+    elif len(possibilities[square]) == 1:
+        d2 = possibilities[square]
+        if not all(remove(possibilities, s2, d2) for s2 in peers[square]): #should be impossible
+            return False
+        
+    for u in units[square]:
+        dplaces = [square for square in u if digit in possibilities[square]] #valid places to put d
+        if len(dplaces) == 0: #no valid places
+            return False
+        elif len(dplaces) == 1:
+            if not assign(possibilities, dplaces[0], digit):
+                return False
+    return possibilities
+
+def display(possibilities):
+    width = 1+max(len(possibilities[square]) for square in squares)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print ''.join(possibilities[r+c].center(width)+('|' if c in '36' else '') for c in cols)
+        if r in 'CF':
+            print line
+    print
+
+def search(possibilities):
+    if possibilities is False:
+        return False
+    if all(len(possibilities[square]) == 1 for square in squares): #if each square has one solution, solved
+        return possibilities
+    n, square = min((len(possibilities[square]), square) for square in squares if len(possibilities[square]) > 1) #find square with min guesses not already solved
+    return find_element(search(assign(possibilities.copy(), square, digit)) for digit in possibilities[square]) #for each square, reduce the possiblities, go through each square (depth first)
+
+def find_element(values):
+    for e in values:
+        if e:
+            return e
+    return False
+
+def solve(board):
+    return search(parse_grid(board))
+
+class SudokuUI(Frame):
+    
     def __draw_puzzle(self):
         self.canvas.delete("numbers")
         for i in xrange(9):
@@ -75,12 +156,17 @@ class SudokuUI(Frame):
             for j in range(9):
                 self.puzzle[i].append(0)
 
+        self.convert()
+
         self.__initUI()
 
     def reset(self):
         for i in range(9):
             for j in range(9):
                 self.puzzle[i][j] = 0
+
+    def convert(self):
+        self.string = ''.join([str(cell) for sub in self.puzzle for cell in sub])
 
     def __draw_cursor(self):
         self.canvas.delete("cursor")
@@ -125,7 +211,8 @@ class SudokuUI(Frame):
         self.__draw_puzzle()
 
     def __solve(self):
-        return
+        self.convert()
+        display(search(parse_grid(self.string)))
 
 def main():
     root = Tk()
